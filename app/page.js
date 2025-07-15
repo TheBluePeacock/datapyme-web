0dac24d8c83f3944731fdae3ddd95cae61f02410
TheBluePeacock <info.thebluepeacock@gmail.com>
2025-07-15 18:26:11 +0200

    actualiza email de contacto

--- app/page.js
+++ app/page.js
@@ -58,7 +58,7 @@ export default function Home() {
         <h2 className="text-2xl font-semibold mb-4">Contacto</h2>
         <p className="text-gray-700 mb-4">
           ¿Te interesa trabajar conmigo? Escríbeme a:{" "}
-          <strong>contacto@datapyme.com</strong>
+          <strong>info.thebluepeacock@gmail.com</strong>
         </p>
         <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
           Enviar Mensaje
